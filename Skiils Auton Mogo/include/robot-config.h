4ad45d3566ac4e96b09982bde548e3e094306c34
7700R <94393717+7700picasso@users.noreply.github.com>
2021-11-16 23:13:25 -0800

    Add files via upload

---
+++ Skiils Auton Mogo/include/robot-config.h
@@ -0,0 +1,26 @@
+using namespace vex;
+
+extern brain Brain;
+
+// VEXcode devices
+extern controller Controller1;
+extern motor leftDrive1;
+extern motor leftDrive2;
+extern motor leftmiddle;
+extern motor rightDrive1;
+extern motor rightDrive2;
+extern motor rightmiddle;
+extern motor amogus;
+extern motor lift1;
+extern digital_out backHook;
+extern digital_out claw;
+extern inertial Inertial21;
+extern digital_out picasso;
+extern inertial Gyro;
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void  vexcodeInit( void );
